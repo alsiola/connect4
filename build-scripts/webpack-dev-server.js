65a231b9db122c03288cf9b26310e54bb0be006d
Alex Young <Alex Young>
2017-01-10 16:28:26 +0000

    change proxy setup to inclusive not exclusive

--- build-scripts/webpack-dev-server.js
+++ build-scripts/webpack-dev-server.js
@@ -49,27 +49,15 @@ compiler.plugin('done', stats => {
 const server = new webpackDevServer(compiler, {
     hot: true,
     historyApiFallback: true,
-    quiet: true
+    quiet: true,
+    proxy: {
+        '/api': {
+            target: 'http://localhost:3000',
+            secure: false
+        }
+    }
 })
 
-const mayProxy = /^(?!\/(index\.html$|.*\.hot-update\.json$|sockjs-node\/|api\/)).*$/;
-server.use(mayProxy,
-    // Pass the scope regex both to Express and to the middleware for proxying
-    // of both HTTP and WebSockets to work without false positives.
-    httpProxyMiddleware(pathname => mayProxy.test(pathname), {
-    target: 3000,
-    logLevel: 'silent',
-    onError: (err, req, res) => {
-        log('Proxy error:', 'red');
-        if (res.writeHead && !res.headersSent) {
-            res.writeHead(500);
-        }
-        res.end('Proxy request failed.');
-    },
-    secure: false,
-    changeOrigin: true
-    })
-);
 
 
 server.listen(8080, 'localhost', (err, result) => {
