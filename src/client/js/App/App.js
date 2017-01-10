a3798b877a5253c593045061695099beb9b9ce51
Alex Young <Alex Young>
2017-01-10 16:33:15 +0000

    remove fluff

--- src/client/js/App/App.js
+++ src/client/js/App/App.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import styles from './App.css';
-import axios from 'axios';
 
 export default class App extends React.Component {
-
-    componentDidMount() {
-        axios.get('/api/currentuser').then(response => {
-            console.log(response.data);
-        });
-    }   
-
     render() {
         return (
-            <div className={styles.yellow}>This is a react app sending the following message... {this.props.message}</div>
+            <div className={styles.yellow}>This is a react app.</div>
         );
     }
 }
